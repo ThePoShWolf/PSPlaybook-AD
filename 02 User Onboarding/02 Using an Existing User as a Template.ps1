0e7dc5916906d8a96427835a4fa951fc4db40d1b
Anthony Howell <anthony@howell-it.com>
2019-01-22 14:15:11 -0800

    Done with the second demo

---
+++ 02 User Onboarding/02 Using an Existing User as a Template.ps1
@@ -0,0 +1,96 @@
+#region Create a template user
+New-ADUser -Name 'Template User' -Enabled $false
+
+# Set all your template properties
+Set-ADUser 'Template User' -StreetAddress '308 Negra Arroyo Lane' -City 'Albuquerque' -State 'New Mexico' -PostalCode '87104'
+
+# Add any groups
+$BaseGroups = 'EveryBody','Main File Share Access'
+ForEach($group in $BaseGroups){
+    Add-ADGroupMember $group -Members 'Template User'
+}
+
+#endregion
+
+#region Creating users from the template
+# Retrieve the template user
+$user = Get-ADUser 'Template User' -Properties StreetAddress,City,State,PostalCode
+
+# Create a single user from that
+New-ADUser 'Walter White' -GivenName 'Walter' -Surname 'White' -Instance $user
+
+# Verify
+Get-ADUser 'Walter White' -Properties StreetAddress,City,State,PostalCode
+
+# Create your spreadsheet users using the template as well
+Function Import-ADUsersFromSpreadsheet {
+    [cmdletbinding(
+        DefaultParameterSetName = 'Plain'
+    )]
+    Param(
+        [ValidatePattern('.*\.xlsx$')]
+        [ValidateNotNullOrEmpty()]
+        [Parameter(
+            ParameterSetName = 'FromTemplate'
+        )]
+        [Parameter(
+            ParameterSetName = 'Plain'
+        )]
+        [string]$PathToSpreadsheet,
+        [Parameter(
+            ParameterSetName = 'FromTemplate',
+            Mandatory = $true
+        )]
+        [Microsoft.ActiveDirectory.Management.ADUser]$TemplateUser,
+        [Parameter(
+            ParameterSetName = 'FromTemplate'
+        )]
+        [ValidateNotNullOrEmpty()]
+        [string[]]$Properties = @('StreetAddress','City','State','PostalCode')
+    )
+    # Hashtable to correlate properties
+    $expectedProperties = @{
+        Name = 'Full Name'
+        GivenName = 'First Name'
+        SurName = 'Last Name'
+        Title = 'Job Title'
+        Department = 'Department'
+        OfficePhone = 'Phone Number'
+    }
+    # Make sure the xlsx exists
+    If(Test-Path $PathToSpreadsheet){
+        $data = Import-Excel $PathToSpreadsheet
+        ForEach($user in $data){
+            # Build a splat
+            $params = @{}
+            ForEach($property in $expectedProperties.GetEnumerator()){
+                # If the new user has the property
+                If($user."$($property.value)".Length -gt 0){
+                    # Add it to the splat
+                    $params["$($property.Name)"] = $user."$($property.value)"
+                }
+            }
+            # Deal with other values
+            If($user.Manager.length -gt 0){
+                $params['Manager'] = $user.Manager.Replace(' ','.')
+            }
+            $params['SamAccountName'] = "$($user.$($expectedProperties['GivenName'])).$($user.$($expectedProperties['SurName']))"
+            # Create the user
+            If($PSCmdlet.ParameterSetName -eq 'Plain'){
+                New-ADUser @params
+            }ElseIf($PSCmdlet.ParameterSetName -eq 'FromTemplate'){
+                $template = Get-ADUser $TemplateUser -Properties $Properties
+                New-ADUser @params -Instance $template
+            }
+        }
+    }
+}
+
+# Verify
+$SpreadSheet = '.\UserUpdate.xlsx'
+$data = Import-Excel $SpreadSheet
+ForEach($user in $data){
+    Get-ADUser "$($user.'First Name').$($user.'Last Name')" -Properties StreetAddress | Select-Object Name,StreetAddress
+}
+
+#endregion
