875c68310c802810eed2c22ce0f1735f91f35040
Anthony Howell <anthony@howell-it.com>
2019-01-22 15:22:10 -0800

    in between stuff

---
+++ 02 User Onboarding/02.9 prep.ps1
@@ -0,0 +1,16 @@
+$params = @{
+    To = 'anthony@howell-it.com'
+    From = 'anthony@howell-it.com'
+    Credential = Get-Credential
+    Subject = 'Email subject line'
+    Body = '<h1>Body</h1><p>this is the paragraph</p>'
+    BodyAsHtml = $true
+    SmtpServer = 'smtp.office365.com'
+    UseSSL = $true
+}
+
+
+foreach($user in $data){
+    Set-ADUser $user.Manager.Replace(' ','.') -EmailAddress 'anthony@howell-it.com'
+}
+$global:From = 'anthony@howell-it.com'
