5d13632150b0b7f37ca054680fb52f4f069ed53a
Anthony Howell <anthony@howell-it.com>
2019-01-22 13:36:42 -0800

    finished 01-01

--- 02 User Onboarding/01 Creating New Users from a Spreadsheet.ps1
+++ 02 User Onboarding/01 Creating New Users from a Spreadsheet.ps1
@@ -3,6 +3,7 @@
     Import-Module ActiveDirectory
 #>
 
+# Region prep work
 # Import the spreadsheet
 $SpreadSheet = '.\UserUpdate.xlsx'
 $Data = Import-Excel $SpreadSheet
@@ -44,3 +45,81 @@ $Params = @{
     OfficePhone = $Data[0].'Phone Number'
     Manager = $Data[0].Manager.Replace(' ','.')
 }
+#endregion
+
+#region As a script
+# Hashtable to correlate properties
+$expectedProperties = @{
+    Name = 'Full Name'
+    GivenName = 'First Name'
+    SurName = 'Last Name'
+    Title = 'Job Title'
+    Department = 'Department'
+    OfficePhone = 'Phone Number'
+}
+
+# Incoming Directory
+$dir = 'C:\Temp\IncomingUsers'
+
+# For each spreadsheet it finds
+ForEach($update in (Get-ChildItem $dir -Filter *.xlsx)){
+    $data = Import-Excel $update.fullname
+    ForEach($user in $data){
+        # Build a splat
+        $params = @{}
+        ForEach($property in $expectedProperties.GetEnumerator()){
+            # If the new user has the property
+            If($user."$($property.value)".Length -gt 0){
+                # Add it to the splat
+                $params["$($property.Name)"] = $user."$($property.value)"
+            }
+        }
+        # Deal with the manager value separate
+        If($user.Manager.length -gt 0){
+            $params['Manager'] = $user.Manager.Replace(' ','.')
+        }
+        # Create the user
+        New-ADUser @params -WhatIf
+    }
+}
+#endregion
+
+#region Create a function
+Function Import-UsersFromSpreadsheet {
+    Param(
+        [ValidatePattern('.*\.xlsx$')]
+        [ValidateNotNullOrEmpty()]
+        [string]$PathToSpreadsheet
+    )
+    # Hashtable to correlate properties
+    $expectedProperties = @{
+        Name = 'Full Name'
+        GivenName = 'First Name'
+        SurName = 'Last Name'
+        Title = 'Job Title'
+        Department = 'Department'
+        OfficePhone = 'Phone Number'
+    }
+    # Make sure the xlsx exists
+    If(Test-Path $PathToSpreadsheet){
+        $data = Import-Excel $PathToSpreadsheet
+        ForEach($user in $data){
+            # Build a splat
+            $params = @{}
+            ForEach($property in $expectedProperties.GetEnumerator()){
+                # If the new user has the property
+                If($user."$($property.value)".Length -gt 0){
+                    # Add it to the splat
+                    $params["$($property.Name)"] = $user."$($property.value)"
+                }
+            }
+            # Deal with the manager value separate
+            If($user.Manager.length -gt 0){
+                $params['Manager'] = $user.Manager.Replace(' ','.')
+            }
+            # Create the user
+            New-ADUser @params -WhatIf
+        }
+    }
+}
+#endregion
