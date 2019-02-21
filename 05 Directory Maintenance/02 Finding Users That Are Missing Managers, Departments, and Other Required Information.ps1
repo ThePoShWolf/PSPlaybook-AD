be7a4471bf18dd1156f384c01123d3d6953cac70
Anthony Howell <anthony@howell-it.com>
2019-02-21 07:56:07 -0800

    better organization pre-record

--- 05 Directory Maintenance/02 Finding Users That Are Missing Managers, Departments, and Other Required Information.ps1
+++ 05 Directory Maintenance/02 Finding Users That Are Missing Managers, Departments, and Other Required Information.ps1
@@ -1,6 +1,7 @@
 #region Filter for missing info
+#region normal filter
 # Define needed info
-$properties = 'Name','Department','Title'
+$properties = 'Name','Department','Title','GivenName','SurName'
 
 # Build a filter
 $filterString = "($($properties[0]) -notlike '*')"
@@ -11,8 +12,16 @@ $filterString
 
 # Get those users
 Get-ADUser -Filter $filterString -Properties $properties | Format-Table $properties
+#endregion
 
-# Manager property
+#region Manager Property
+# We can filter for specific managers
+Get-ADUser -Filter {Manager -eq 'Walter White'}
+
+# But not empty manager
+Get-ADUser -Filter {Manager -eq ''}
+
+# Using an LDAPFilter
 Get-ADUser -LDAPFilter "(!manager=*)" -Properties Manager | Format-Table Name,Manager
 
 # Combine both into an LDAP filter
@@ -22,11 +31,13 @@ For($x=1;$x -lt $properties.count; $x++){
     $ldapFilter += "(!$($properties[$x])=*)"
 }
 $ldapFilter += ')'
+$ldapFilter
+
 Get-ADUser -LDAPFilter $ldapFilter -Properties $properties | Format-Table $properties
 
 # One issue with using an LDAP filter
-Get-ADUser -LDAPFilter '(!surname=*)'
-Get-ADUser -LDAPFilter '(!sn=*)'
+Get-ADUser -LDAPFilter '(!surname=*)' | Select-Object GivenName,SurName
+Get-ADUser -LDAPFilter '(!sn=*)' | Select-Object GivenName,SurName
 
 # Build a hashtable of values
 # https://stackoverflow.com/questions/41447372/is-there-a-complete-list-of-active-directory-attributes-and-a-mapping-to-ldap
@@ -39,6 +50,7 @@ $LDAPNameConstants | ForEach-Object {
 }
 
 # Now
+$LDAPPropertyMap
 $LDAPPropertyMap['SurName']
 
 # New filter
@@ -47,8 +59,12 @@ For($x=1;$x -lt $properties.count; $x++){
     $ldapFilter += "(!$($LDAPPropertyMap[$properties[$x]])=*)"
 }
 $ldapFilter += ')'
-Get-ADUser -LDAPFilter $ldapFilter -Properties $properties | Format-Table $properties
+$ldapFilter
 
+Get-ADUser -LDAPFilter $ldapFilter -Properties $properties | Format-Table $properties
+#endregion
+
+#region Filter to the left
 # Compare that to Where-Object
 Measure-Command {
     Get-ADUser -LDAPFilter $ldapFilter -Properties $properties
@@ -59,17 +75,19 @@ Measure-Command {
         -not $_.Name -or
         -not $_.Department -or
         -not $_.Title -or
-        -not $_.Manager
+        -not $_.Manager -or
+        -not $_.GivenName -or
+        -not $_.SurName
     }
 }
-
+#endregion
 #endregion
 
 #region Make it into a function
 Function Get-ADUsersMissingInfo {
     [cmdletbinding()]
     Param (
-        [string[]]$Properties = @('Name','Department','Title','Manager')
+        [string[]]$Properties = @('Name','Department','Title','Manager','GivenName','SurName')
     )
     # Build our property map
     $ADAssembly = [Microsoft.ActiveDirectory.Management.ADEntity].Assembly
@@ -93,7 +111,7 @@ Function Get-ADUsersMissingInfo {
 Get-ADUsersMissingInfo
 
 # Custom
-Get-ADUsersMissingInfo -Properties OfficePhone
+Get-ADUsersMissingInfo -Properties OfficePhone | Select-Object Name,OfficePhone
 
 # Create an Excel sheet
 $exportExcelParams = @{
@@ -101,7 +119,8 @@ $exportExcelParams = @{
     TableName = 'Props'
     TableStyle = 'Light1'
 }
-Get-ADUsersMissingInfo | Export-Excel .\MissingProperties.xlsx -Title 'Missing Properties' @exportExcelParams
+Get-ADUsersMissingInfo | Select-Object $properties |
+Export-Excel .\MissingProperties.xlsx -Title 'Missing Properties' @exportExcelParams
 
 Import-Excel .\MissingProperties.xlsx -StartRow 2
 
