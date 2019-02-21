34bbe5abc50ccffb34d566147e5fc101549acede
Anthony Howell <anthony@howell-it.com>
2019-02-21 08:35:06 -0800

    Minor updates pre-record

--- 05 Directory Maintenance/04 Normalizing Phone Numbers.ps1
+++ 05 Directory Maintenance/04 Normalizing Phone Numbers.ps1
@@ -1,6 +1,7 @@
 #region The problem
 Get-ADUser -Filter {OfficePhone -like '*'} -Properties OfficePhone | Select-Object Name,OfficePhone
 
+#region Resolve it with PowerShell
 # Decide on a format
 # I'll be fancy: (XXX) XXX-XXXX
 
@@ -11,23 +12,27 @@ Get-ADuser -Filter {OfficePhone -like '(*) *-*'} -Properties OfficePhone | Forma
 $Examples = '1234567890','(123) 4567890','123-456-7890'
 
 # Normalize
-foreach ($n in $Examples){
-    $n -replace '[^\d]',''
+foreach ($num in $Examples){
+    $num -replace '[^\d]',''
 }
 
 # Add the wanted syntax
-foreach ($n in $Examples){
-    $tmp = $n -replace '[^\d]',''
-    "($($tmp.Substring(0,3))) $($tmp.Substring(3,3))-$($tmp.Substring(5,4))"
+foreach ($num in $Examples){
+    $tmp = $num -replace '[^\d]',''
+    If($tmp.Length -eq 10){
+        "($($tmp.Substring(0,3))) $($tmp.Substring(3,3))-$($tmp.Substring(6,4))"
+    }Else{
+        Write-Host 'Invalid number'
+    }
 }
-
+#endregion
 #endregion
 
 #region Make the change to all users
 $users = Get-ADUser -Filter {OfficePhone -notlike '(*) *-*'} -Properties OfficePhone
 foreach ($user in $users){
     $tmp = $user.OfficePhone -replace '[^\d]',''
-    $user.OfficePhone = "($($tmp.Substring(0,3))) $($tmp.Substring(3,3))-$($tmp.Substring(5,4))"
+    $user.OfficePhone = "($($tmp.Substring(0,3))) $($tmp.Substring(3,3))-$($tmp.Substring(6,4))"
     Set-ADUser -Instance $user
 }
 
