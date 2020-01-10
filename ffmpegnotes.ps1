c5358b705a840da59509ee854c92a9f85661a16a
Anthony Howell <anthony@howell-it.com>
2020-01-10 10:06:55 -0800

    final commit, added more notes and get-mediainfo

--- ffmpegnotes.ps1
+++ ffmpegnotes.ps1
@@ -32,9 +32,16 @@ Function Join-ffmpegMp4 {
         }
     }
 }
+Function Get-MediaFileInfo {
+    Param(
+        [string]$File,
+        [string]$ffprobe = 'C:\Users\Anthony\Downloads\ffmpeg-20190926-87ddf9f-win64-static\ffmpeg-20190926-87ddf9f-win64-static\bin\ffprobe.exe'
+    )
+    (& $ffprobe -v quiet -of json -show_format -show_streams $file) -join '' | convertfrom-json
+}
 
 $dir = 'C:\Users\Anthony\OneDrive - Howell IT\TechSnips\Courses\PSPlaybook\Edited'
-$dirs = Get-ChildItem $dir -Recurse -Directory | ?{(Get-ChildItem $_.FullName).count -eq 2}
+$dirs = Get-ChildItem $dir -Recurse -Directory | ?{(Get-ChildItem $_.FullName -File).count -eq 2}
 foreach ($d in $dirs) {
     $files = Get-ChildItem $d.FullName -Filter *.mp4 | Sort -Descending
     # powershell-playbook-automating-active-directory-M5-C1.mp4
@@ -43,6 +50,26 @@ foreach ($d in $dirs) {
         $m = $Matches.m
     }
     $c = $split[9]
+    #Write-Host $d.fullname
     #Write-Host "m: $m, c: $c"
     Join-ffmpegMp4 -Files $files -OutputFile "$($d.fullname)\powershell-playbook-automating-active-directory-M$m-C$c.mp4"
 }
+
+$dir = 'C:\Users\Anthony\OneDrive - Howell IT\TechSnips\Courses\PSPlaybook\Edited'
+$dirs = Get-ChildItem $dir -Recurse -Directory -FollowSymLink | ?{(Get-ChildItem $_.FullName -File).count -eq 3}
+foreach ($d in $dirs){
+    $file = Get-ChildItem $d.FullName -Filter *.mp4 | ?{$_.Name -match '^powershell\-playbook\-automating\-active\-directory\-M\d\d\-C\d\d\.mp4$'}
+    $info = Get-MediaFileInfo $file.FullName
+    $video = $info.streams | ?{$_.codec_type -eq 'video'}
+    if($video.width -ne '1280') {
+        Write-Host "Converting $($file.name)"
+        $file.name | Out-File .\fileupdates.txt -Append
+        $outfile = "$($file.Directory)\$($file.BaseName)_720.mp4"
+        #Write-Host $outfile
+        & $ffmpeg -i "$($file.FullName)" -vf scale=-1:720 -c:v libx264 -crf 18 -preset veryslow -c:a copy $outfile
+    }
+}
+
+$path = 'C:\Users\Anthony\OneDrive - Howell IT\TechSnips\Courses\PSPlaybook\Edited\09 - Making You Scripts Approachable\02\powershell-playbook-automating-active-directory-M09-C02.mp4'
+$path = 'C:\Users\Anthony\OneDrive - Howell IT\TechSnips\Courses\PSPlaybook\Edited\08 - Extending into the Cloud\02\powershell-playbook-automating-active-directory-M08-C02.mp4'
+& $ffmpeg -i "$path" -vf scale=-1:720 -c:v libx264 -crf 18 -preset veryslow -c:a copy .\test.mp4
